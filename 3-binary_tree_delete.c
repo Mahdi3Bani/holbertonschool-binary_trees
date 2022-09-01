0b47f1180a687079ac39caa029e66d8edd676968
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-01 16:07:21 +0100

    delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,8 +9,8 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree == NULL)
 		return;
 
-	deleteTree(tree->left);
-	deleteTree(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
 	free(tree);
 }
