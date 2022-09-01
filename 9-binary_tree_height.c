2138e7e6cc0a9b37ed0b7879433febfe796d5305
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-02 00:39:45 +0100

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left_height = tree_height(tree->left);
-	right_height = tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 	if (left_height >= right_height)
 		return (left_height);
 	else
