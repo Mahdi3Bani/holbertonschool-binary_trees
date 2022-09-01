20a3431faaa8b87548296792391a71dced83b7f1
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-01 16:16:41 +0100

    leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,14 +2,14 @@
 /**
  * binary_tree_is_leaf - check if the node is a leaf
  * @node: node to check
- *
+ * Reurn: 1 if node is a leaf, otherwise 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 
 	if (!node)
 		return (0);
-	if (node->left || node->right)
+	if (node->left && node->right)
 		return (1);
 	return (0);
 }
