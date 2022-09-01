52943326a112e4c7110c219d75b6dc7dffbcdec8
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-02 00:50:32 +0100

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,17 @@
 #include "binary_trees.h"
+/**
+ * max - find max
+ * @a: var
+ * @b: var
+ * Return: max
+ *
+ */
+size_t max(size_t a, size_t b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
 
 /**
  * binary_tree_height - measures the height of a binary tree
@@ -8,15 +21,9 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-	size_t left_height, right_height;
-
 	if (!tree || (!tree->right && !tree->left))
 		return (0);
 
-	left_height = binary_tree_height(tree->left) ;
-	right_height = binary_tree_height(tree->right);
-	if (left_height >= right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	return (max(binary_tree_height(tree->left),
+	binary_tree_height(tree->right)) + 1);
 }
