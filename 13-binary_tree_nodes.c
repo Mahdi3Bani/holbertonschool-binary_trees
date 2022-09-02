de9fd9cc0eaf8cde1c5ae9d1b575dcc721ab166e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-02 14:29:35 +0100

    nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,7 +12,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (!tree->left || !tree->right)
-		return (1);
+		return (0);
 	return (binary_tree_nodes(tree->left) +
 			binary_tree_nodes(tree->right));
 }
