da88317eb5b59fb8eca6ac29a48fdfef327e7b23
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-02 15:31:58 +0100

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,33 @@
 #include "binary_trees.h"
+/**
+ * max - find max
+ * @a: var
+ * @b: var
+ * Return: max
+ *
+ */
+size_t max(size_t a, size_t b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
 
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: return height or 0 if null
+ */
+int _binary_tree_height(const binary_tree_t *tree)
+{
+
+	if (!tree || (!tree->right && !tree->left))
+		return (0);
+
+	return (max(_binary_tree_height(tree->left),
+				_binary_tree_height(tree->right)) +
+			1);
+}
 
 /**
  * binary_tree_balance - measures the balance of a binary tree
@@ -9,9 +37,10 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 
+	int right, left;
 	if (!tree)
 		return (0);
-
-	return (binary_tree_balance(tree->left) -
-	binary_tree_balance(tree->right) + 1);
+	right = _binary_tree_height(tree->right);
+	left = _binary_tree_height(tree->left);
+	return (left - right);
 }
