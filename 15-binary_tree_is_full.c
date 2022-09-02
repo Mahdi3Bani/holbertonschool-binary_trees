a7387e647e66edbe51d35504d8656b0dea037abb
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-02 21:08:14 +0100

    full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,7 +10,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left && tree->right)
-		return (1);
+		return (0);
 
 	return (binary_tree_is_full(tree->left));
 	return (binary_tree_is_full(tree->right));
