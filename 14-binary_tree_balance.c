84c07528383202515bd54acb15b67f3f14833c55
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-02 15:18:41 +0100

    balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_balance - measures the balance of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance
+ * Return: return balance or 0 if null
+ */
+size_t binary_tree_balance(const binary_tree_t *tree)
+{
+
+	if (!tree || (!tree->right && !tree->left))
+		return (0);
+
+	return (binary_tree_balance(tree->left) -
+	binary_tree_balance(tree->right));
+}
