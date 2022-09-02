773d5cb2d91e6f43ce7e92e6d1c4b5c52ba51844
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-02 14:19:00 +0100

    leaf

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
+ * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  * Return: return height or 0 if null
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 
 	if (!tree)
 		return (0);
 
 	if (!tree->left && !tree->right)
-		return (binary_tree_size(tree->left) +
-				1 + binary_tree_size(tree->right));
+		return (binary_tree_leaves(tree->left) +
+				1 + binary_tree_leaves(tree->right));
 }
